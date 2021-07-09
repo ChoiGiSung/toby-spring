5750696fcf49fc2e47f546daf4aa80909f9aa33e
gisungchoi <chlrltjd159@naver.com>
2021-07-09 16:48:07 +0900

    feat : ApplicationContext(ioc 컨테이너)에 등록(설정 정보)

--- src/main/java/com/example/toby/chapter1/DaoFactory.java
+++ src/main/java/com/example/toby/chapter1/DaoFactory.java
@@ -1,13 +1,22 @@
 package com.example.toby.chapter1;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 설정을 담당하는 class
+ * */
+
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao(){
         return new UserDao(connectionMaker());
     }
 
-    private ConnectionMaker connectionMaker() {
-        ConnectionMaker maker = new DConnectionMaker();
-        return maker;
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
