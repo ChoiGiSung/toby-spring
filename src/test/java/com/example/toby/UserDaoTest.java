d8bcb75c7aaf4420e49a60df759eb4826042edc5
gisungchoi <chlrltjd159@naver.com>
2021-07-11 23:26:55 +0900

    feat : 예외 test
    - JdbcSQLNonTransientException이 나지만 라이브러리에 없어서 Exception으로 처리

--- src/test/java/com/example/toby/UserDaoTest.java
+++ src/test/java/com/example/toby/UserDaoTest.java
@@ -3,6 +3,7 @@ package com.example.toby;
 import com.example.toby.chapter2.DaoFactory;
 import com.example.toby.chapter2.User;
 import com.example.toby.chapter2.UserDao;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -14,8 +15,8 @@ import static org.assertj.core.api.Assertions.*;
 
 public class UserDaoTest {
 
-    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-    UserDao userDao = context.getBean("userDao",UserDao.class);
+    private ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    private UserDao userDao = context.getBean("userDao",UserDao.class);
 
     @BeforeEach
     void deleteAll() throws SQLException, ClassNotFoundException {
@@ -26,17 +27,45 @@ public class UserDaoTest {
     @Test
     void add() throws SQLException, ClassNotFoundException {
 
-        User user = user();
-        userDao.add(user);
+        User user1 = user("1");
+        User user2 = user("2");
+        userDao.add(user1);
+        userDao.add(user2);
 
-        User DBUser = userDao.get("1");
+        User DBUser1 = userDao.get("1");
+        User DBUser2 = userDao.get("2");
 
-        assertThat(user.getId()).isEqualTo(DBUser.getId());
+        assertThat(user1.getId()).isEqualTo(DBUser1.getId());
+        assertThat(user2.getId()).isEqualTo(DBUser2.getId());
 
     }
 
-    User user(){
-        return new User("1","name","password");
+    @Test
+    void count() throws SQLException, ClassNotFoundException {
+        User user1 = user("1");
+        User user2 = user("2");
+        User user3 = user("3");
+
+        userDao.add(user1);
+        assertThat(1).isEqualTo(userDao.getCount());
+
+        userDao.add(user2);
+        assertThat(2).isEqualTo(userDao.getCount());
+
+        userDao.add(user3);
+        assertThat(3).isEqualTo(userDao.getCount());
+    }
+
+    @Test
+    void getUserFailure() throws SQLException, ClassNotFoundException {
+        Assertions.assertThrows(Exception.class,() -> {
+            userDao.get("4");
+        });
+
+    }
+
+    User user(String id){
+        return new User(id,"name","password");
     }
 
 }
