a2b2fd550d1630eae3f2301ccfa9e028785f1daa
gisungchoi <chlrltjd159@naver.com>
2021-07-20 16:36:11 +0900

    test : 리플랙션을 통해 메소드

---
+++ src/test/java/com/example/toby/chapter6/ReflectionTest.java
@@ -0,0 +1,27 @@
+package com.example.toby.chapter6;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionTest {
+
+    @Test
+    void invokeMethod() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        String name = "Spring";
+
+        assertThat(6).isEqualTo(name.length());
+
+        Method lengthMethod = String.class.getMethod("length");
+        assertThat(6).isEqualTo(lengthMethod.invoke(name));
+
+        assertThat('S').isEqualTo(name.charAt(0));
+        Method charAtMethod = String.class.getMethod("charAt", int.class);
+
+        assertThat('S').isEqualTo(charAtMethod.invoke(name,0));
+    }
+}
