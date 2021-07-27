f6e5c63782c65863c473418e0f8f4abf992d7750
gisungchoi <chlrltjd159@naver.com>
2021-07-27 21:46:36 +0900

    feat : 동시성 문제 해결을 위한 ConcurrentHashMapSqlRegistry 정의
    - 기존의 hashMap은 sql 수정시 동시성 문제가 발생할 수 있음

---
+++ src/main/java/com/example/toby/chapter7/SqlUpdateFailureException.java
@@ -0,0 +1,4 @@
+package com.example.toby.chapter7;
+
+public class SqlUpdateFailureException extends RuntimeException {
+}
--- src/main/java/com/example/toby/chapter7/UpdatableSqlRegistry.java
+++ src/main/java/com/example/toby/chapter7/UpdatableSqlRegistry.java
@@ -1,10 +1,12 @@
 package com.example.toby.chapter7;
 
+import org.springframework.jdbc.object.SqlUpdate;
+
 import java.util.Map;
 
 public interface UpdatableSqlRegistry extends SqlRegistry{
 
-    void updateSql(String key,String sql) throws SqlRetrievalFailureException;
-    void updateSql(Map<String,String> sqlmap) throws SqlRetrievalFailureException;
+    void updateSql(String key,String sql) throws SqlUpdateFailureException;
+    void updateSql(Map<String,String> sqlmap) throws SqlUpdateFailureException;
 
 }
