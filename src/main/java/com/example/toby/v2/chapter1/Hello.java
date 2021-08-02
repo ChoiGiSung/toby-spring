764a8486845d8b851c38a256458513700fd812eb
gisungchoi <chlrltjd159@naver.com>
2021-08-02 22:03:59 +0900

    test : BeanDefinition을 정의해서 빈 등록
    - BeanDefinition을 구현한 RootBeanDefinition class를 통해 빈을 등록한다
    - 컨테이너에 등록된 빈은 같은 타입이라도 다른 오브젝트를 같는다. 왜냐면 클래스를 빈으로 만드는 것이 아닌 오브젝트를 만드는 것이기에
    - 등록된 BeanDefinition은 디폴트 하나, 새롭게 정의한거 하나 총 2개이다

--- src/main/java/com/example/toby/v2/chapter1/Hello.java
+++ src/main/java/com/example/toby/v2/chapter1/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
     }
 
     public String sayHello(){
-        return "Hello" + name;
+        return "Hello " + name;
     }
 
     public String getName() {
--- src/test/java/com/example/toby/v2/chapter1/IocTest.java
+++ src/test/java/com/example/toby/v2/chapter1/IocTest.java
@@ -1,6 +1,8 @@
 package com.example.toby.v2.chapter1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.support.StaticApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,5 +16,25 @@ class IocTest {
 
         Hello hello1 = context.getBean("hello1", Hello.class);
         assertThat(hello1).isNotNull();
+    }
+
+    @Test
+    void BeanDefinition을_이용한_빈등록(){
+        StaticApplicationContext context = new StaticApplicationContext();
+
+        BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+        helloDef.getPropertyValues().addPropertyValue("name","Spring");
+
+        context.registerBeanDefinition("hello2",helloDef);
+        context.registerSingleton("hello1",Hello.class);
+
+        Hello hello1 = context.getBean("hello1", Hello.class);
+        Hello hello2 = context.getBean("hello2", Hello.class);
+
+        assertThat(hello2.sayHello()).isEqualTo("Hello Spring");
+        assertThat(hello1).isNotEqualTo(hello2);
+
+        assertThat(context.getBeanFactory().getBeanDefinitionCount()).isEqualTo(2);
+
     }
 }
